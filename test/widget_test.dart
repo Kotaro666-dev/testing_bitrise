989a2f4bc32e7b8e954f308963aa6e1924f0de39
Kotaro Kamashima <kotaro0726@gmail.com>
2022-12-10 12:27:25 +0900

    Modify: Refactor codes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:testing_bitrise/app.dart';
 
 import 'package:testing_bitrise/main.dart';
 
